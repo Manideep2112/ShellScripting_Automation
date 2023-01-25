6f59021dd892375c8ade459bb7112d38a95539bb
Manideep2112 <105777074+Manideep2112@users.noreply.github.com>
2023-01-25 12:24:04 +0530

    Update automation6.sh

--- automation6.sh
+++ automation6.sh
@@ -9,4 +9,11 @@ $2
 Regards,
 ShineInCareer 
 MESSAGE_END
-
+--- end----
+https://stackoverflow.com/questions/57410259/how-to-send-an-email-using-sendmail-command-in-linux
+# sendmail command line optons:
+# -i - do not treat lines starting with dot specially
+# -t - read recipients lists from message headers: TO,CC,BCC
+# -v - use verbose mode (describe what is happening) 
+#
+# Empty lines separates mail headers from mail body
